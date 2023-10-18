e8444ba3b432a94c2267b6c94162bb605a7e9f44
kens1ne <tanndph25572@fpt.edu.vn>
2023-10-18 10:16:18 +0700

    Update API

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -1,4 +1,4 @@
-export const { BASE_URL } = process.env
+export const BASE_URL = process.env.API_URL
 export const API_TIMEOUT = Number(process.env.API_TIMEOUT) || 30000
 export const API_PREFIX = '/api/v1'
 export const SECRET_KEY = process.env.SECRET_KEY || ''
