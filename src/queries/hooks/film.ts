e85bf31446ea14b2fbab1797acc5b6f5008907f8
tonyanh <trunganh123cx@gmail.com>
2023-10-31 14:43:26 +0700

    feat:update_film_cms

--- src/queries/hooks/film.ts
+++ src/queries/hooks/film.ts
@@ -3,7 +3,7 @@ import { TResApi, TResApiErr, TResDataListApi } from '@configs/interface.config'
 import { checkAuth } from '@src/libs/localStorage'
 import { notification } from 'antd'
 import { useMutation, useQuery } from 'react-query'
-import { removeFilmById } from '../apis'
+import { getFilmById, removeFilmById, updateFilmById } from '../apis'
 import { queryClient } from '..'
 import {
           createFilm,
@@ -11,7 +11,7 @@ import {
 
 } from '../apis'
 import { TQueryLayout } from '@src/modules'
-import { LIST_FILM } from '../keys'
+import { DETAIL_FILM, LIST_FILM } from '../keys'
 
 /**
  *
@@ -60,3 +60,29 @@ export const useMutationRemoveFilmById = () =>
         },
     })
 
+
+/**
+ * @method useMutationUpdateFilmById
+ * @returns
+ */
+export const useMutationUpdateFilmById = () => {
+    return useMutation(({ id, data }: { id: string; data: any }) => updateFilmById(id, data), {
+        onSuccess: (res: TResApi) => {
+            queryClient.refetchQueries([LIST_FILM])
+            notification.success({ message: NSuccess, description: res?.message })
+        },
+        onError: (error: TResApiErr) => {
+            notification.error({ message: NError, description: error?.message })
+        }
+    })
+}
+
+/**
+ * @method useQueryGetFilmById
+ * @param {string}id
+ * @returns
+ */
+
+export const useQueryGetFilmById = (id: string) => {
+    return useQuery<TResApi<any>>([DETAIL_FILM, id], () => getFilmById(id), { enabled: !!id })
+}
